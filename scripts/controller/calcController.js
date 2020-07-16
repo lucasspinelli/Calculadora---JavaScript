f8e629820f7f35ba0a64eb6fea75462d97dc2bb4
Lucas Levi <lucas.lsl12@gmail.com>
2020-07-15 22:30:24 -0300

    Adicionado os cases da calculadora, assim que colocar validação para converter e concanetar, aparecera os numeros no display

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,6 +1,8 @@
 class CalcController {
 
     constructor(){
+
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display"); // El é colocado por convensão para se referir ao elemento
         this._dateEl = document.querySelector("#data");
@@ -31,14 +33,92 @@ class CalcController {
 
     }
 
+    clearAll(){
+
+        this._operation = [];
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+    addOperation(value){
+
+        this._operation.push(value);
+
+        console.log(this._operation);
+
+    }
+
+
+    execBtn(value){
+        switch (value){
+
+            case 'ac':
+                this.clearAll();
+                 break;
+            case 'ce':
+                this.clearEntry();
+                 break;
+            case 'soma':
+                
+                 break;
+            case 'subtracao':
+                
+                 break;
+            case 'divisao':
+            
+                 break;
+            case 'multiplicacao':
+                
+                 break;
+            case 'porcento':
+                
+                 break;
+            case 'igual':
+                
+                 break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+
+        }
+    }
+
     initButtomEvents(){
 
        let buttons = document.querySelectorAll("#buttons > g, #parts > g"); // > é o seletor de filhos
 
        buttons.forEach((btn, index)=>{
            this.addEventListenerAll(btn, "click drag", e=>{
-               console.log(btn.className.baseVal.replace("btn-",""));// traz só o nome da clase, fica mais clean
-           });
+              
+                let textBtn = btn.className.baseVal.replace("btn-","");// traz só o nome da clase, fica mais clean
+            
+                this.execBtn(textBtn);
+           
+            });
 
            this.addEventListenerAll(btn," mouseover mouseup mousedown", e=>{
                btn.style.cursor = "pointer";
@@ -46,6 +126,7 @@ class CalcController {
        });
 
     }
+
 
     setDisplayDateTime(){
         this.displayDate = this.currentDate.toLocaleDateString(this._locale);
